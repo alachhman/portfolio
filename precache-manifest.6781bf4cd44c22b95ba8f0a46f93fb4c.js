dec494cb996e56c425e6e2d09a56f99ccbba5834
alachhman <33943685+alachhman@users.noreply.github.com>
2020-08-11 16:52:19 -0400

    Updates

---
+++ precache-manifest.6781bf4cd44c22b95ba8f0a46f93fb4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78617fc9b5f540888867fd2d9b1dfc1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccff143bc4ec897fe8e7",
+    "url": "/static/css/main.a3d94c8d.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccff143bc4ec897fe8e7",
+    "url": "/static/js/main.0dde55c6.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
